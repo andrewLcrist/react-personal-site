f08a76e54926ee8a062d4123329bf2f291fd29da
Andrew Crist <andrew.l.crist@gmail.com>
2018-04-02 12:54:50 -0500

    Initial setup

--- lib/components/Application.jsx
+++ lib/components/Application.jsx
@@ -1,54 +1,18 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
-import { signIn, signOut } from '../firebase'
-import { split } from 'lodash';
-
-import SearchMovie from './SearchMovie';
-import Upcoming from './Upcoming';
-import MyMovies from './MyMovies';
-import NotFound from './NotFound';
-import Navigation from './Navigation';
-import Login from './Login';
-import firebase from '../firebase';
 
 export default class Application extends Component{
   constructor(){
     super()
     this.state = {
-      user: null,
-      movieResults: [],
-      showMenu: false
     }
   }
 
-  componentWillMount() {
-    firebase.auth().onAuthStateChanged(user => this.setState({ user }));
-  }
-
-  toggleMobileMenu() {
-    this.setState({ showMenu: !this.state.showMenu})
-  }
 
   render() {
     return(
-      <Router>
-        <div>
-          { this.state.user ?
-            <div>
-              <Navigation
-                user={this.state.user}
-                showMenu={this.state.showMenu}
-                toggleMobileMenu={this.toggleMobileMenu.bind(this)}
-              />
-              <Route exact path="/" render={() => <MyMovies user={this.state.user}/>}/>
-              <Route exact path="/explore" render={() => <Upcoming user={this.state.user}/>} />
-              <Route exact path="/search" render={() => <SearchMovie user={this.state.user}/>} />
-            </div>
-          :
-            <Login />
-          }
-        </div>
-      </Router>
+      <div>
+        <h1>Test</h1>
+      </div>
     )
   }
 }
